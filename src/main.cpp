dbc80936d7fdb1638cec875bbdfcc2c6e8ac8a36
Youngho Cho <dominoyh5@gmail.com>
2023-10-24 15:52:48 +0900

    Update: Add memory class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
+class Memory {
+	uint8_t memory[0xFFFFFFFF];
+};
+
 int main() {
+	Memory memory;
 	cout << "asdf\n";
 
 	return 0;
