e7906d9c8e46a13bc9b1b34d25513b5f168c390b
Youngho Cho <dominoyh5@gmail.com>
2023-10-24 21:24:10 +0900

    Fix: addi immediate to signed

--- include/computer.h
+++ include/computer.h
@@ -15,8 +15,44 @@ class Computer {
 	Memory *memory;
 	Registers registers;
 
-	bool step_syscall();
+	void step_add(Instruction instruction);
 	void step_addi(Instruction instruction);
+	void step_addiu(Instruction instruction);
+	void step_addu(Instruction instruction);
+	void step_and(Instruction instruction);
+	void step_andi(Instruction instruction);
+	void step_beq(Instruction instruction);
+	void step_bne(Instruction instruction);
+	void step_j(Instruction instruction);
+	void step_jal(Instruction instruction);
+	void step_jr(Instruction instruction);
+	void step_lbu(Instruction instruction);
+	void step_lhu(Instruction instruction);
+	void step_ll(Instruction instruction);
+	void step_lui(Instruction instruction);
+	void step_lw(Instruction instruction);
+	void step_nor(Instruction instruction);
+	void step_or(Instruction instruction);
+	void step_ori(Instruction instruction);
+	void step_slt(Instruction instruction);
+	void step_slti(Instruction instruction);
+	void step_sltiu(Instruction instruction);
+	void step_sltu(Instruction instruction);
+	void step_sll(Instruction instruction);
+	void step_srl(Instruction instruction);
+	void step_sb(Instruction instruction);
+	void step_sh(Instruction instruction);
+	void step_sw(Instruction instruction);
+	void step_sub(Instruction instruction);
+	void step_subu(Instruction instruction);
+	void step_div(Instruction instruction);
+	void step_divu(Instruction instruction);
+	void step_mfhi(Instruction instruction);
+	void step_mflo(Instruction instruction);
+	void step_mult(Instruction instruction);
+	void step_multu(Instruction instruction);
+	void step_sra(Instruction instruction);
+	bool step_syscall();
 };
 
 #endif
--- src/computer.cpp
+++ src/computer.cpp
@@ -22,6 +22,140 @@ bool Computer::step() {
 	return 0;
 }
 
+void Computer::step_add(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_addi(Instruction instruction) {
+	int32_t temp = 0;
+	temp += registers.signed_get(instruction.rs);
+	temp += (int16_t)instruction.immediate;
+	registers.set(instruction.rt, temp);
+}
+
+void Computer::step_addiu(Instruction instruction) {
+}
+
+void Computer::step_addu(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_and(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_andi(Instruction instruction) {
+}
+
+void Computer::step_beq(Instruction instruction) {
+}
+
+void Computer::step_bne(Instruction instruction) {
+}
+
+void Computer::step_j(Instruction instruction) {
+}
+
+void Computer::step_jal(Instruction instruction) {
+}
+
+void Computer::step_jr(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_lbu(Instruction instruction) {
+}
+
+void Computer::step_lhu(Instruction instruction) {
+}
+
+void Computer::step_ll(Instruction instruction) {
+}
+
+void Computer::step_lui(Instruction instruction) {
+}
+
+void Computer::step_lw(Instruction instruction) {
+}
+
+void Computer::step_nor(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_or(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_ori(Instruction instruction) {
+}
+
+void Computer::step_slt(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_slti(Instruction instruction) {
+}
+
+void Computer::step_sltiu(Instruction instruction) {
+}
+
+void Computer::step_sltu(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_sll(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_srl(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_sb(Instruction instruction) {
+}
+
+void Computer::step_sh(Instruction instruction) {
+}
+
+void Computer::step_sw(Instruction instruction) {
+}
+
+void Computer::step_sub(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_subu(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_div(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_divu(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_mfhi(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_mflo(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_mult(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_multu(Instruction instruction) {
+	(void)instruction;
+}
+
+void Computer::step_sra(Instruction instruction) {
+	(void)instruction;
+}
+
 bool Computer::step_syscall() {
 	switch (registers.unsigned_get(V0)) {
 		case 1:		// print integer
@@ -36,9 +170,3 @@ bool Computer::step_syscall() {
 	return false;
 }
 
-void Computer::step_addi(Instruction instruction) {
-	int32_t temp = 0;
-	temp += registers.signed_get(instruction.rs);
-	temp += instruction.immediate;
-	registers.set(instruction.rt, temp);
-}
