f5b6a1156ce52f8ec948661b0e5ac4362468a16a
Youngho Cho <dominoyh5@gmail.com>
2023-11-02 00:15:01 +0900

    Add: test asm code

---
+++ testcase/fact_func.s
@@ -0,0 +1,27 @@
+		addi $a0, $zero, 5
+		jal fact
+		add $a0, $v0, $zero
+		addi $v0, $zero, 1
+		syscall
+		addi $a0, $zero, 10
+		addi $v0, $zero, 11
+		syscall
+		addi $v0, $zero, 10
+		syscall
+
+fact:	addi $sp, $sp, -8
+		sw $ra, 4($sp)
+		sw $a0, 0($sp)
+		slti $t0, $a0, 1
+		beq $t0, $zero, L1
+		addi $v0, $zero, 1
+		addi $sp, $sp, 8
+		jr $ra
+L1:		addi $a0, $a0, -1
+		jal fact
+		lw $a0, 0($sp)
+		lw $ra, 4($sp)
+		addi $sp, $sp, 8
+		mult $v0, $a0
+		mflo $v0
+		jr $ra
