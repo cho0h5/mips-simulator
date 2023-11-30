95574451122f8497c4f832ff0160b637f045dc10
Youngho Cho <dominoyh5@gmail.com>
2023-11-30 19:14:06 +0900

    #include <cstdint>

--- include/memory.h
+++ include/memory.h
@@ -1,108 +1,109 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <cstdint>
 #include <iostream>
 
 #include <registers.h>
 
 enum InstructionFormat {
-	R_FORMAT,
-	I_FORMAT,
-	J_FORMAT,
+  R_FORMAT,
+  I_FORMAT,
+  J_FORMAT,
 };
 
 enum OpCode {
-	OP_ADD		= 0b000000,
-	OP_ADDI		= 0b001000,
-	OP_ADDIU	= 0b001001,
-	OP_ADDU		= 0b000000,
-	OP_AND		= 0b000000,
-	OP_ANDI		= 0b001100,
-	OP_BEQ		= 0b000100,
-	OP_BNE		= 0b000101,
-	OP_J		= 0b000010,
-	OP_JAL		= 0b000011,
-	OP_JR		= 0b000000,
-	OP_LBU		= 0b100100,
-	OP_LHU		= 0b100101,
-	OP_LUI		= 0b001111,
-	OP_LW		= 0b100011,
-	OP_NOR		= 0b000000,
-	OP_OR		= 0b000000,
-	OP_ORI		= 0b001101,
-	OP_SLT		= 0b000000,
-	OP_SLTI		= 0b001010,
-	OP_SLTIU	= 0b001011,
-	OP_SLTU		= 0b000000,
-	OP_SLL		= 0b000000,
-	OP_SRL		= 0b000000,
-	OP_SB		= 0b101000,
-	OP_SH		= 0b101001,
-	OP_SW		= 0b101011,
-	OP_SUB		= 0b000000,
-	OP_SUBU		= 0b000000,
-	OP_DIV		= 0b000000,
-	OP_DIVU		= 0b000000,
-	OP_MFHI		= 0b000000,
-	OP_MFLO		= 0b000000,
-	OP_MULT		= 0b000000,
-	OP_MULTU	= 0b000000,
-	OP_SRA		= 0b000000,
-	OP_SYSCALL	= 0b000000,
+  OP_ADD = 0b000000,
+  OP_ADDI = 0b001000,
+  OP_ADDIU = 0b001001,
+  OP_ADDU = 0b000000,
+  OP_AND = 0b000000,
+  OP_ANDI = 0b001100,
+  OP_BEQ = 0b000100,
+  OP_BNE = 0b000101,
+  OP_J = 0b000010,
+  OP_JAL = 0b000011,
+  OP_JR = 0b000000,
+  OP_LBU = 0b100100,
+  OP_LHU = 0b100101,
+  OP_LUI = 0b001111,
+  OP_LW = 0b100011,
+  OP_NOR = 0b000000,
+  OP_OR = 0b000000,
+  OP_ORI = 0b001101,
+  OP_SLT = 0b000000,
+  OP_SLTI = 0b001010,
+  OP_SLTIU = 0b001011,
+  OP_SLTU = 0b000000,
+  OP_SLL = 0b000000,
+  OP_SRL = 0b000000,
+  OP_SB = 0b101000,
+  OP_SH = 0b101001,
+  OP_SW = 0b101011,
+  OP_SUB = 0b000000,
+  OP_SUBU = 0b000000,
+  OP_DIV = 0b000000,
+  OP_DIVU = 0b000000,
+  OP_MFHI = 0b000000,
+  OP_MFLO = 0b000000,
+  OP_MULT = 0b000000,
+  OP_MULTU = 0b000000,
+  OP_SRA = 0b000000,
+  OP_SYSCALL = 0b000000,
 };
 
 enum Funct {
-	FT_ADD		= 0b100000,
-	FT_ADDU		= 0b100001,
-	FT_AND		= 0b100100,
-	FT_JR		= 0b001000,
-	FT_NOR		= 0b100111,
-	FT_OR		= 0b100101,
-	FT_SLT		= 0b101010,
-	FT_SLTU		= 0b101011,
-	FT_SLL		= 0b000000,
-	FT_SRL		= 0b000010,
-	FT_SUB		= 0b100010,
-	FT_SUBU		= 0b100011,
-	FT_DIV		= 0b011010,
-	FT_DIVU		= 0b011011,
-	FT_MFHI		= 0b010000,
-	FT_MFLO		= 0b010010,
-	FT_MULT		= 0b011000,
-	FT_MULTU	= 0b011001,
-	FT_SRA		= 0b000011,
-	FT_SYSCALL	= 0b001100,
+  FT_ADD = 0b100000,
+  FT_ADDU = 0b100001,
+  FT_AND = 0b100100,
+  FT_JR = 0b001000,
+  FT_NOR = 0b100111,
+  FT_OR = 0b100101,
+  FT_SLT = 0b101010,
+  FT_SLTU = 0b101011,
+  FT_SLL = 0b000000,
+  FT_SRL = 0b000010,
+  FT_SUB = 0b100010,
+  FT_SUBU = 0b100011,
+  FT_DIV = 0b011010,
+  FT_DIVU = 0b011011,
+  FT_MFHI = 0b010000,
+  FT_MFLO = 0b010010,
+  FT_MULT = 0b011000,
+  FT_MULTU = 0b011001,
+  FT_SRA = 0b000011,
+  FT_SYSCALL = 0b001100,
 };
 
 struct Instruction {
-	InstructionFormat	format;
-	OpCode				opcode;
-	RegisterName		rs;
-	RegisterName		rt;
-	RegisterName		rd;
-	uint8_t				shamt;
-	Funct				funct;
-	uint16_t			immediate;
-	uint32_t			address;
+  InstructionFormat format;
+  OpCode opcode;
+  RegisterName rs;
+  RegisterName rt;
+  RegisterName rd;
+  uint8_t shamt;
+  Funct funct;
+  uint16_t immediate;
+  uint32_t address;
 };
 
 class Memory {
-	public:
-	bool load_program(std::string filename);
-	Instruction fetch_instruction(int pc);
-	static void print_instruction(Instruction instruction);
+public:
+  bool load_program(std::string filename);
+  Instruction fetch_instruction(int pc);
+  static void print_instruction(Instruction instruction);
 
-	uint8_t load_byte_unsigned(uint32_t address);
-	uint16_t load_halfword_unsigned(uint32_t address);
-	uint32_t load_word(uint32_t address);
-	void store_byte(uint32_t address, uint8_t value);
-	void store_halfword(uint32_t address, uint16_t value);
-	void store_word(uint32_t address, uint32_t value);
+  uint8_t load_byte_unsigned(uint32_t address);
+  uint16_t load_halfword_unsigned(uint32_t address);
+  uint32_t load_word(uint32_t address);
+  void store_byte(uint32_t address, uint8_t value);
+  void store_halfword(uint32_t address, uint16_t value);
+  void store_word(uint32_t address, uint32_t value);
 
-	private:
-	uint8_t memory[0x100000000];
+private:
+  uint8_t memory[0x100000000];
 
-	static InstructionFormat categorize_format(OpCode opcode);
+  static InstructionFormat categorize_format(OpCode opcode);
 };
 
 #endif
--- include/registers.h
+++ include/registers.h
@@ -1,66 +1,67 @@
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
+#include <cstdint>
 #include <iostream>
 
 enum RegisterName {
-	ZERO = 0,
-	AT,
-	V0,
-	V1,
-	A0,
-	A1,
-	A2,
-	A3,
-	T0,
-	T1,
-	T2,
-	T3,
-	T4,
-	T5,
-	T6,
-	T7,
-	S0,
-	S1,
-	S2,
-	S3,
-	S4,
-	S5,
-	S6,
-	S7,
-	T8,
-	T9,
-	K0,
-	K1,
-	GP,
-	SP,
-	FP,
-	RA,
+  ZERO = 0,
+  AT,
+  V0,
+  V1,
+  A0,
+  A1,
+  A2,
+  A3,
+  T0,
+  T1,
+  T2,
+  T3,
+  T4,
+  T5,
+  T6,
+  T7,
+  S0,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  T8,
+  T9,
+  K0,
+  K1,
+  GP,
+  SP,
+  FP,
+  RA,
 };
 
 class Registers {
-	public:
-	Registers();
-	uint32_t unsigned_get(RegisterName register_name);
-	int32_t signed_get(RegisterName register_name);
-	void set(RegisterName register_name, uint32_t value);
+public:
+  Registers();
+  uint32_t unsigned_get(RegisterName register_name);
+  int32_t signed_get(RegisterName register_name);
+  void set(RegisterName register_name, uint32_t value);
 
-	uint32_t get_pc();
-	void set_pc(uint32_t address);
-	void increase_pc();
-	void i_format_jump(int16_t branch_addr);
-	void j_format_jump(uint32_t branch_addr);
+  uint32_t get_pc();
+  void set_pc(uint32_t address);
+  void increase_pc();
+  void i_format_jump(int16_t branch_addr);
+  void j_format_jump(uint32_t branch_addr);
 
-	void set_lo(uint32_t value);
-	void set_hi(uint32_t value);
-	uint32_t get_lo();
-	uint32_t get_hi();
+  void set_lo(uint32_t value);
+  void set_hi(uint32_t value);
+  uint32_t get_lo();
+  uint32_t get_hi();
 
-	private:
-	uint32_t general_registers[32];
-	uint32_t pc;
-	uint32_t lo;
-	uint32_t hi;
+private:
+  uint32_t general_registers[32];
+  uint32_t pc;
+  uint32_t lo;
+  uint32_t hi;
 };
 
 #endif
